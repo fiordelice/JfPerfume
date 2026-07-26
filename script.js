041ddfb708cd70b7951c116c29e1462ab632261a
fiordelice <127434566+fiordelice@users.noreply.github.com>
2026-07-26 19:17:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -449,7 +449,7 @@ const products = [
   category: 'loção corporal',
   description: 'Loção corporal hidratante e perfumada com fragrância oriental gourmand doce e envolvente. Notas de baunilha, jasmim e musk. Pote rosa luxuoso com tampa prateada detalhada, elegante e sofisticado. Hidrata profundamente e deixa a pele macia e perfumada. Ideal para usar após o banho ou combinando com o perfume da mesma linha.',
   price: 100.00,
-  image: 'wa_image_7559976990564847631',
+  image: 'yara.jpeg',
 },
 ];
 
